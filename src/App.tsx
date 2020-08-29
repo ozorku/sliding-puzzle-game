4d3681957a0e49c9e604a711e74719d634faa514
adavize <adavize@thepoint.africa>
2020-08-29 22:13:19 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [shuffledNumbers, setShuffledNumbers] = useState([]);
+
   useEffect(() => {
     shuffleNumbers();
   }, []);
